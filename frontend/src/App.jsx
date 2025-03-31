5ea7a499aac8a545bda759c0fcdb57710f5f0493
Swagatam Biswas <63900743+BiswasSwagatam@users.noreply.github.com>
2025-03-31 15:39:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        `https://https://ai-pdf-assistant.onrender.com/upload?question=${encodeURIComponent(question)}`,
+        `https://ai-pdf-assistant.onrender.com/upload?question=${encodeURIComponent(question)}`,
         formData,
         {
           headers: {
